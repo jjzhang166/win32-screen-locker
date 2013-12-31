025746fad599e71c7a246d481dc5790ad48525f9
feng32tc@gmail.com <feng32tc@gmail.com@aa155af3-a7ed-f37e-83cf-81db689354aa>
2013-12-31 09:35:40 +0000

    Release 1.0.0

---
+++ tags/1.0.0/ScreenLock/Resource.h
@@ -0,0 +1,14 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDM_TRAY                                100
+#define IDC_OLD_PASSWORD                        1003
+#define ICO_MAIN                                4096
+#define IDM_LOCK_NOW                            40000
+#define IDM_SET_PASSWORD                        40001
+#define IDC_SET_PASSWORD                        40002
+#define IDM_EXIT                                40002
+#define IDC_CONFIRM                             40003
+#define IDC_NEW_PASSWORD                        40003
+#define IDC_NEW_CONFIRM                         40004
